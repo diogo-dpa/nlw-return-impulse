408caadc9313a33d48de449ef15146b27298e848
Diogo Almazan <diogodpa@outlook.com>
2022-05-07 00:00:40 -0300

    Configuração de cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { routes } from "./routes";
 
 const app = express();
 
+// Forma de controle de segurança para acesso ao frontend
+app.use(cors());
 // Antes de processar, veja da requisição e converta para json
 app.use(express.json());
 app.use(routes);
