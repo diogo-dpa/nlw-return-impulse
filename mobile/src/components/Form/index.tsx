bbb5a70243129ed7999c0b8f3ed384368af6c8d3
Diogo Almazan <diogodpa@outlook.com>
2022-05-17 22:25:50 -0300

    Lib react-native-view-shot e funcionalidade de screenshot

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -1,5 +1,6 @@
 import { ArrowLeft } from "phosphor-react-native";
-import React from "react";
+import React, { useState } from "react";
+import { captureScreen } from "react-native-view-shot";
 
 import { View, TextInput, Image, Text, TouchableOpacity } from "react-native";
 import { FeedbackType } from "../../components/Widget";
@@ -15,7 +16,21 @@ interface Props {
 }
 
 export function Form({ feedbackType }: Props) {
+	const [screenshot, setScreenshot] = useState<string | null>(null);
 	const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+	function handleScreenshot() {
+		captureScreen({
+			format: "jpg",
+			quality: 0.8,
+		})
+			.then((uri) => setScreenshot(uri))
+			.catch((error) => console.log(error));
+	}
+
+	function handleScreenshotRemove() {
+		setScreenshot(null);
+	}
 
 	return (
 		<View style={styles.container}>
@@ -43,9 +58,9 @@ export function Form({ feedbackType }: Props) {
 
 			<View style={styles.footer}>
 				<ScreenshotButton
-					onTakeShot={() => {}}
-					onRemoveShot={() => {}}
-					screenshot=""
+					onTakeShot={handleScreenshot}
+					onRemoveShot={handleScreenshotRemove}
+					screenshot={screenshot}
 				/>
 				<Button isLoading={false} />
 			</View>
