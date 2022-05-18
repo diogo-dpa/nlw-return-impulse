e2333a4b5909b3c440f8c4fb9da1a9379ca73ac4
Diogo Almazan <diogodpa@outlook.com>
2022-05-17 22:53:49 -0300

    Libs de conversão para base64, axios para chamadas, navegação nos componentes e tudo funcionando

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeft } from "phosphor-react-native";
 import React, { useState } from "react";
 import { captureScreen } from "react-native-view-shot";
+import * as FileSystem from "expo-file-system";
 
 import { View, TextInput, Image, Text, TouchableOpacity } from "react-native";
 import { FeedbackType } from "../../components/Widget";
@@ -10,13 +11,22 @@ import { ScreenshotButton } from "../../components/ScreenshotButton";
 import { styles } from "./styles";
 import { theme } from "../../theme";
 import { feedbackTypes } from "../../utils/feedbackTypes";
+import { api } from "../../libs/api";
 
 interface Props {
 	feedbackType: FeedbackType;
+	onFeedbackCanceled: () => void;
+	onFeedbackSent: () => void;
 }
 
-export function Form({ feedbackType }: Props) {
+export function Form({
+	feedbackType,
+	onFeedbackCanceled,
+	onFeedbackSent,
+}: Props) {
+	const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 	const [screenshot, setScreenshot] = useState<string | null>(null);
+	const [comment, setComment] = useState("");
 	const feedbackTypeInfo = feedbackTypes[feedbackType];
 
 	function handleScreenshot() {
@@ -32,10 +42,36 @@ export function Form({ feedbackType }: Props) {
 		setScreenshot(null);
 	}
 
+	async function handleSendFeedback() {
+		if (isSendingFeedback) {
+			return;
+		}
+
+		setIsSendingFeedback(true);
+
+		const screenshotBase64 =
+			screenshot &&
+			(await FileSystem.readAsStringAsync(screenshot, { encoding: "base64" }));
+
+		try {
+			await api.post("/feedbacks", {
+				type: feedbackType,
+				screenshot: `data:image/png;base64, ${screenshotBase64}`,
+				comment,
+			});
+
+			onFeedbackSent();
+		} catch (err) {
+			console.log(err);
+		} finally {
+			setIsSendingFeedback(false);
+		}
+	}
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.header}>
-				<TouchableOpacity>
+				<TouchableOpacity onPress={onFeedbackCanceled}>
 					<ArrowLeft
 						size={24}
 						weight="bold"
@@ -54,6 +90,8 @@ export function Form({ feedbackType }: Props) {
 				style={styles.input}
 				placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
 				placeholderTextColor={theme.colors.text_secondary}
+				autoCorrect={false}
+				onChangeText={setComment}
 			/>
 
 			<View style={styles.footer}>
@@ -62,7 +100,7 @@ export function Form({ feedbackType }: Props) {
 					onRemoveShot={handleScreenshotRemove}
 					screenshot={screenshot}
 				/>
-				<Button isLoading={false} />
+				<Button isLoading={isSendingFeedback} onPress={handleSendFeedback} />
 			</View>
 		</View>
 	);
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 import { TouchableOpacity } from "react-native";
 import { ChatTeardropDots } from "phosphor-react-native";
@@ -15,11 +15,24 @@ import { feedbackTypes } from "../../utils/feedbackTypes";
 export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
+	const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+	const [feedbackSent, setFeedbackSent] = useState(false);
+
 	const bottomSheetRef = useRef<BottomSheet>(null);
 
 	function handleOpen() {
 		bottomSheetRef.current?.expand();
 	}
+
+	function handleRestartFeedback() {
+		setFeedbackType(null);
+		setFeedbackSent(false);
+	}
+
+	function handleFeedbackSent() {
+		setFeedbackSent(true);
+	}
+
 	return (
 		<>
 			<TouchableOpacity style={styles.button} onPress={handleOpen}>
@@ -36,7 +49,21 @@ function Widget() {
 				backgroundStyle={styles.modal}
 				handleIndicatorStyle={styles.indicator}
 			>
-				<Form feedbackType="BUG" />
+				{feedbackSent ? (
+					<Success onSendAnotherFeedback={handleRestartFeedback} />
+				) : (
+					<>
+						{feedbackType ? (
+							<Form
+								feedbackType={feedbackType}
+								onFeedbackCanceled={handleRestartFeedback}
+								onFeedbackSent={handleFeedbackSent}
+							/>
+						) : (
+							<Options onFeedbackTypeChanged={setFeedbackType} />
+						)}
+					</>
+				)}
 			</BottomSheet>
 		</>
 	);
